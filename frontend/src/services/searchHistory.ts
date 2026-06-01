8428de12f5638b61e3ff2c5966e0c35a0802b907
unknown <mssanjay97@gmail.com>
2026-05-31 19:17:41 -0700

    removed comments

--- frontend/src/services/searchHistory.ts
+++ frontend/src/services/searchHistory.ts
@@ -3,11 +3,5 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function searchHistory(query: string, userId: string) {
-    return axios.post(
-        `${API_BASE_URL}/api/v1/queries`,
-        {
-            query,
-            userId
-        }
-    );
+    return axios.post(`${API_BASE_URL}/api/v1/queries`, {query, userId});
 }
